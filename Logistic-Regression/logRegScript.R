284625e2dd6cae839e14b595d4c69a1c1cf1b8d5
Sabin <spradh@users.noreply.github.com>
2016-12-06 14:10:31 -0500

    Update logRegScript.R

--- Logistic-Regression/logRegScript.R
+++ Logistic-Regression/logRegScript.R
@@ -3,42 +3,49 @@ View(heart)
 nrow(heart)
 heart<-heart[,-1]
 
+summary(heart)
+
 #Cleaning Response Variable
 heart$AHD<-heart$AHD=="Yes"
 
+levels(heart$Slope)
 levels(heart$ChestPain)
 levels(heart$Thal)
 
 #Creating Dummy Variables for Chestpain
+#ChestPain Typical is Control group
 heart$chestpainAsymtomatic<-heart$ChestPain=="asymptomatic"
 heart$chestpainNonanginal<-heart$ChestPain=="nonanginal"
 heart$chestpainNontypical<-heart$ChestPain=="nontypical"
-heart$chestpainTypical<-heart$ChestPain=="typical"
 heart<-heart[,-3]
 
 #Creating dummy variables for Thal
-heart$thalFixed<-heart$Thal=="fixed"
+#Thal=fixed is control group
 heart$thalNormal<-heart$Thal=="normal"
 heart$thanReversable<-heart$Thal=="reversable"
 heart<-heart[,-12] #removing Thal
 
 #Creating dummy variables for CA
-heart$ca1<-heart$Ca==0
+#CA=0 is control group
 heart$ca1<-heart$Ca==1
 heart$ca2<-heart$Ca==2
 heart$ca3<-heart$Ca==3
 heart<-heart[,-11]
 
 #Creating dummy variables for slope
-heart$slope1<-heart$Slope==1
+#Slope=1 is control group
 heart$slope2<-heart$Slope==2
 heart$slope3<-heart$Slope==3
 heart<-heart[,-10]
 
 View(heart)
 
-
+cor(heart,method="pearson")
 train<-heart[1:250,]
 test<-heart[251:nrow(heart),]
 View(train)
 View(test)
+
+#Traning data set
+model<-glm(AHD~.,family=binomial,data=train)
+summary(model)
