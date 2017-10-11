7c24bdb47d5cbaba0788aadb1d2b38bca84ef06c
Sabin <spradh@users.noreply.github.com>
2017-10-11 14:22:55 -0400

    Update RScript.r

--- Project 1/RScript.r
+++ Project 1/RScript.r
@@ -2,60 +2,64 @@
 #Importing Data
 ######################
 data<-read.csv("wdbc.data",header = FALSE)
-names(data)<-c('id',"outcome","time",
+names(data)<-c('id',"diagnosis",
                "radiusM","textureM","perimeterM","areaM","smoothnessM","compactnessM","concavityM","concavePointsM","symmetryM","fractalDimensionM",
                "radiusSE","textureSE","perimeterSE","areaSE","smoothnessSE","compactnessSE","concavitySE","concavePointsSE","symmetrySE","fractalDimensionSE",
-               "radiusW","textureW","perimeterW","areaW","smoothnessW","compactnessW","concavityW","concavePointsW","symmetryW","fractalDimensionW",
-               "tumorSize","lymphNodeStatus")
+               "radiusW","textureW","perimeterW","areaW","smoothnessW","compactnessW","concavityW","concavePointsW","symmetryW","fractalDimensionW")
 
 # Encoding categorical data
-dataset$outcome <- factor(dataset$outcome, levels = c('M', 'B'), labels = c(1, 0))
-dataset$outcome <- as.numeric(as.character(dataset$outcome))
-
+data$diagnosis = factor(data$diagnosis,
+                    levels = c('M', 'B'),
+                    labels = c(1, 0))
+id<-data[,1]
+data<-data[,-1]        #taking the patient ids aways for modelling purpose
 header<-names(data)
-View(data)
 dim(data)
+
 ######################
 #Training Data
 ######################
-set.seed(10)
+set.seed(101)
+
 #Assisning training data set size as 75% of total data
 training_size<-round(dim(data)[1]*.75)
 training_size
+
 #Creating a random index sample 
 trainingIndex<-sample(1:dim(data)[1], size = training_size,replace = FALSE)
-trainingIndex
+
 #Assigning the training set
 training<-data[trainingIndex,]
-View(training)
 dim(training)
-nonFactor<-training[,1:3]
-View(nonFactor)
-factors<-data.matrix(training[,4:35], rownames.force = NA)
-View(factors)
-factorMean<-colMeans(factors)
-factorMean
-factorSd<-colSds(factors) #matrixStats Package was used for the colSds function
-factorSd
-factors<-scale(factors,center = TRUE,scale = TRUE)
-View(factors)
-training<-cbind(factors,nonFactor[,2]=='R')
+
+#separating nonfactors before scaling
+trainingDiagnosis<-training[,1]
+trainingFactors<-data.matrix(training[,2:31], rownames.force = NA)
+
+
+#preProcess
+#install.packages('caret')
+library(caret)
+prep<-preProcess(trainingFactors,method=c('center','scale'))
+summary(prep)
+trainingFactors<-predict(prep,trainingFactors)
+training<-cbind.data.frame(trainingFactors,trainingDiagnosis)
 View(training)
+
 
 ######################
 #Test Data
 ######################
 test<-data[-trainingIndex,]
 View(test)
-testNonFactors<-test[,1:3]
-testFactors<-test[,4:35]
-View(testFactors)
-(testFactors[1,]-factorMean)
-for(i in 1:dim(test)[2]){
-  testFactors[i,]<-(testFactors[i,]-factorMean)/factorSd
-}
+dim(test)
+sapply(test,class)
+testDiagnosis<-test[,1]
+testFactors<-data.matrix(test[,2:31], rownames.force = NA)
+class(testFactors)
 
-View(testFactors)
+test<-predict(prep,testFactors)
+test<-cbind.data.frame(testFactors,testDiagnosis)
 
 dim(test)
 View(test)
